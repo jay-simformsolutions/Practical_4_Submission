2a0a94be6ef70a425c19a3a8fbecc29627652572
jay <jay.akbari@simformsolutions.com>
2023-02-06 11:29:15 +0530

    Change return type of alert function and use text theme from themedata

---
+++ practical_4_signup_and_sigin_UI/lib/CommonMethodClass/clip_react.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ClipClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    // TODO: implement getClip
+    Path path = Path();
+    path.lineTo(0, size.height - 40);
+    path.quadraticBezierTo(
+        size.width / 4, size.height, size.width / 2, size.height);
+    path.quadraticBezierTo(size.width - (size.width / 4), size.height,
+        size.width, size.height - 40);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
+}
